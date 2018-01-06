2910de5caba12936d3144848f339b773e5947908
TAKAHASHI Yuto <ytaka23dev@gmail.com>
2018-01-06 19:43:55 +0900

    Publish the artifacts to GitHub Pages

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
         ]
     },
     output: {
-        path: path.resolve(__dirname + '/dist'),
+        path: path.resolve(__dirname + '/docs'),
         filename: '[name].js'
     },
     module: {
